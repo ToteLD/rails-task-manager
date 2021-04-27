e4725584ecad2c43ec49bd7c82295de543c84e47
thomas le devendec <thomas.ledevendec@hotmail.fr>
2021-04-27 13:01:45 +0200

    edit and delete added -no style

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new'
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  post 'tasks', to: 'tasks#create'
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # get 'tasks/new', to: 'tasks#new'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
 
+  resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
