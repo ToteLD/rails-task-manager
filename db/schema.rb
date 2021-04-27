93727de4d33a44cef625b1b177f3f55a4f6a7385
thomas le devendec <thomas.ledevendec@hotmail.fr>
2021-04-27 12:25:43 +0200

    Create task added

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_27_093540) do
+ActiveRecord::Schema.define(version: 2021_04_27_101209) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.string "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
